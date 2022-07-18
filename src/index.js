d031a95990c92f4af51efd418a37b9e76df6d756
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2022-07-18 08:29:45 -0700

    modifying layout

--- src/index.js
+++ src/index.js
@@ -2,15 +2,33 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import BasicExample from "./BasicExample.js";
+import BasicExample from './BasicExample.js';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
-    <App />
-    <BasicExample />
+    <Container>
+      <Row>
+        <Col>
+          <BasicExample />
+        </Col>
+
+        <Col>
+          <Row>
+            <App />
+          </Row>
+
+          <Row>
+            <App />
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   </StrictMode>
 );
